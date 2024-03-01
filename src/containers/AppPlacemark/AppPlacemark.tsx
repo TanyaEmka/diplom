2933500caf9f4c08b4a7a9fca61d105efc254872
unknown <tanyaemelyanova2015@gmail.com>
2024-03-01 11:11:36 +0300

    change app placemark, add examples in mocks

--- src/containers/AppPlacemark/AppPlacemark.tsx
+++ src/containers/AppPlacemark/AppPlacemark.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import { Placemark } from "@pbe/react-yandex-maps";
 
-import location from '../../assets/icons/location/location.svg';
-
 interface AppPlacemarkProps {
     id: number,
     name: string,
@@ -12,20 +10,15 @@ interface AppPlacemarkProps {
 }
 
 export const AppPlacemark: React.FC<AppPlacemarkProps> = ({
-    id,
     name, wood,
     point,
 }) => {
-
-    const [ width, height ] = [ 32, 50 ];
-
     return (
         <Placemark
             geometry={point}
             options={{
-                iconLayout: 'default#image',
-                iconImageHref: location,
-                iconImageSize: [width, height],
+                preset: 'islands#circleDotIcon',
+                iconColor: '#A455C9',
             }}
             properties={{
                 hintContent: [name, wood].join(', '),
